927db9ef7b9fb7ef8ad0a5da5664743e2343f209
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-12-04 23:08:43 -0500

    Create Temperature Model

---
+++ TemperatureConverter/TemperatureModel.swift
@@ -0,0 +1,13 @@
+//
+//  TemperatureModel.swift
+//  TemperatureConverter
+//
+//  Created by Maribel Montejano on 11/22/17.
+//  Copyright © 2017 Maribel Montejano. All rights reserved.
+//
+
+import Foundation
+
+class TemperatureModel {
+    
+}
