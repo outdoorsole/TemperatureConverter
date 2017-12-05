2c01c358cd7fbcd63dc3edeb36dfe7ad5a93bb19
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-12-05 01:47:59 -0500

    Dismiss 2nd VC with switch button

---
+++ TemperatureConverter/FtoCViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FtoCViewController.swift
+//  TemperatureConverter
+//
+//  Created by Maribel Montejano on 12/5/17.
+//  Copyright © 2017 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class FtoCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func switchButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
